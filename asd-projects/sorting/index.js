27bccd6604d0b9209f744ad5763dea1608c3ebf1
BDrew44 <andrewblondeau6@gmail.com>
2025-06-24 15:15:49 +0000

    saving sorting exercise

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -15,27 +15,63 @@ The CSS ids you will work with are:
 
 // TODO 2: Implement bubbleSort
 async function bubbleSort(array) {
-    for (var i = 0; i < array.length - 1; i++) {
-    for (var j = array.length - 1; j > i; j--) {
-      if (array[j] < array[j - 1]) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = array.length - 1; j > i; j--) {
+      if (array[j].value < array[j - 1].value) {
         swap(array, j, j - 1);
+        updateCounter(bubbleCounter);
+        await sleep();
       }
     }
   }
-
 }
 
 // TODO 3: Implement quickSort
-
+async function quickSort(array, left, right) {
+  if (right - left > 0) {
+    let index = await partition(array, left, right);
+    if (left < index - 1) {
+      await quickSort(array, left, index - 1);
+    }
+    if (index < right) {
+      await quickSort(array, index, right);
+    }
+  }
+}
 
 // TODOs 4 & 5: Implement partition
 
+async function partition(array, left, right) {
+  let pivot = array[Math.floor((left + right) / 2)].value;
+
+  while (left < right) {
+    // 5a) Move left index to the right as long as its value is less than the pivot
+    while (array[left].value < pivot) {
+      left++;
+    }
+    // 5b) Move right index to the left as long as its value is greater than the pivot
+    while (array[right].value > pivot) {
+      right--;
+    }
+    // 5c) Swap if left is still less than right
+    if (left < right) {
+      swap(array, left, right);
+      updateCounter(quickCounter);
+      await sleep();
+      left++;
+      right--;
+    }
+  }
+
+  return left + 1;
+}
 
 // TODO 1: Implement swap
-function swap (array, i, j) {
-array[i] = array[j];
-array[j] = array[i];
-    drawSwap(array, i, j);
+function swap(array, i, j) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+  drawSwap(array, i, j);
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -45,22 +81,24 @@ array[j] = array[i];
 //////////////////////////// HELPER FUNCTIONS /////////////////////////
 
 // this function makes the program pause by SLEEP_AMOUNT milliseconds whenever it is called
-function sleep(){
-    return new Promise(resolve => setTimeout(resolve, SLEEP_AMOUNT));
+function sleep() {
+  return new Promise((resolve) => setTimeout(resolve, SLEEP_AMOUNT));
 }
 
 // This function draws the swap on the screen
-function drawSwap(array, i, j){
-    let element1 = array[i];
-    let element2 = array[j];
+function drawSwap(array, i, j) {
+  let element1 = array[i];
+  let element2 = array[j];
 
-    let temp = parseFloat($(element1.id).css("top")) + "px";
+  let temp = parseFloat($(element1.id).css("top")) + "px";
 
-    $(element1.id).css("top", parseFloat($(element2.id).css("top")) + "px");
-    $(element2.id).css("top", temp);
+  $(element1.id).css("top", parseFloat($(element2.id).css("top")) + "px");
+  $(element2.id).css("top", temp);
 }
 
 // This function updates the specified counter
-function updateCounter(counter){
-    $(counter).text("Move Count: " + (parseFloat($(counter).text().replace(/^\D+/g, '')) + 1));
+function updateCounter(counter) {
+  $(counter).text(
+    "Move Count: " + (parseFloat($(counter).text().replace(/^\D+/g, "")) + 1)
+  );
 }
