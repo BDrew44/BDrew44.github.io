be8a0f485bb56eb6de4799981922da75e1f9feb4
IoT <iot@Operations-Mac-mini.local>
2026-02-10 19:28:52 -0600

    test

--- iot-projects/are-you-being-served/fetchServer.js
+++ iot-projects/are-you-being-served/fetchServer.js
@@ -1,1 +1,22 @@
 // fetchServer.js file
+const fetch = require("node-fetch");
+
+const http = require("http");
+
+const port = 3000; // Port to listen on
+
+http.createServer(requestListenerFunction).listen(port);
+
+http
+  .createServer(async function (req, res) {
+    const response = await fetch("http://Bdrew44.github.io");
+
+    console.log(response);
+
+    if (response.ok) {
+      console.log("response recieved");
+      console.log(response.status);
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+  })
+  .listen(port);
